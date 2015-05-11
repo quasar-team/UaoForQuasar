4c9ecc5b170e99318b0ec9bfa0c475d22048359e
Piotr Nikiel <piotr.nikiel@cern.ch>
2015-05-11 16:04:53 +0000

    wip
    git-svn-id: svn+ssh://svn.cern.ch/reps/atlas-pnikiel/UaObjectify@435 4525493e-7705-40b1-a816-d608a930855b

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import pyuaf
+from pyuaf.client           import Client
+from pyuaf.client.settings  import ClientSettings, SessionSettings
+from pyuaf.util             import Address, NodeId, ExpandedNodeId, opcuaidentifiers
+from pyuaf.util.errors      import UafError, ConnectionError
+import pdb
+from  pyuaf.util import nodeclasses
+
+server_uri='urn:AtlasDcs:OpcUaCanOpenServer'
+server_address='opc.tcp://pcatldcsp11.cern.ch:4841'
+client = None
+unfold = False
+
+
+def connect( ):
+    global client
+    cs = ClientSettings("myClient", [server_address])
+    client = Client(cs)
+    rootNode = Address( NodeId(opcuaidentifiers.OpcUaId_RootFolder, 0), server_uri )
+    result=client.browse ([ rootNode ])
+    
+
+
 
 
 class ObjectifiedNode():
@@ -7,18 +30,35 @@ class ObjectifiedNode():
         self.a = addr
         self.contained = {}
         self.variables = {}
-        # here -- scan it
+        #  execute browse
+        browse_result = client.browse([ self.a ])
+        for ri in xrange(len(browse_result.targets[0].references)):
+            ref = browse_result.targets[0].references[ri]
+            print ref.displayName
+            
+            # now we have information - node class
+            if ref.nodeClass == nodeclasses.Variable:
+                self.variables[str(ref.displayName.text())] = Address(ExpandedNodeId(ref.nodeId))
+            elif ref.nodeClass == nodeclasses.Object:
+                if unfold:
+                    self.contained[str(ref.displayName.text())] = ObjectifiedNode( Address(ExpandedNodeId(ref.nodeId)) )
+            
         
     
     def __getattr__(self,name):
         if self.variables.has_key(name):
-            # todo  -- read from the variable
-            return 'has this variable'
+            a = self.variables[name]
+            r = client.read([a])
+            return r.targets[0].data.value
         elif self.contained.has_key(name):
-            # todo -- return the object
-            return 'an object'
+            return self.contained[name]
         else:
             raise AttributeError
         
 
-root = ObjectifiedNode('a')
+
+
+def get_object( string_a, ns ):
+    n = NodeId( string_a, ns )
+    a = Address( n, server_uri )
+    return ObjectifiedNode(a)
